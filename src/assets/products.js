d263168290d88f8b472ac04e6aa5e0e279f6cabe
Arinze Ogbuniba <niklaus.oa@gmail.com>
2024-07-05 21:38:48 +0100

    Add more products

--- src/assets/products.js
+++ src/assets/products.js
@@ -1,6 +1,10 @@
 import cake from "./images/chocolate-cake.jpg";
 import lemonTart from "./images/lemon-tart.jpg";
-import applePie from "./images/apple-pie.jpg";
+import applePie from "./images/apple-pie.webp";
+import velvetCupcake from "./images/red-velvet-cupcake.jpg"
+import muffin from "./images/blueberry-muffin.jpg"
+import doughnuts from "./images/doughnuts.jpg"
+
 
 const Products = [
   {
@@ -20,6 +24,24 @@ const Products = [
     name: "Apple Pie",
     price: 20,
     image: applePie,
+  },
+  {
+    id: 4,
+    name: "Blueberry Muffin",
+    price: 3,
+    image: muffin,
+  },
+  {
+    id: 5,
+    name: "Glazed Doughnuts",
+    price: 4,
+    image: doughnuts, 
+  },
+  {
+    id: 6,
+    name: "Red Velvet Cupcake",
+    price: 4,
+    image: velvetCupcake, 
   },
 ];
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,9 +11,9 @@ const Home = () => {
       </div>
 
       {/* Products */}
-      <section className="flex mt-9 max-w-4xl mx-auto gap-4 flex-wrap justify-center">
+      <section className="flex mt-9 max-w-4xl mx-auto gap-4 flex-wrap justify-start">
         {products.map((product) => (
-          <div key={product.id} className="basis-36 grow max-w-48 border border-gray-300 rounded-lg">
+          <div key={product.id} className="basis-40 max-w-60 grow border border-gray-300 rounded-lg">
             <img
               className="w-full h-36 object-cover object-center rounded-tl-lg rounded-tr-lg"
               src={product.image}
@@ -22,6 +22,7 @@ const Home = () => {
             <div className="py-3 px-3">
               <p>{product.name}</p>
               <p className="font-semibold text-2xl">{`$${product.price}.00`}</p>
+              <button className="mt-6 bg-red-600 text-white py-2 px-4 rounded-lg w-full hover:bg-red-700 transition-colors duration-200" type="button">Add to cart</button>
             </div>
           </div>
         ))}
