51f0ae4fe0bd4710fe51d9225182066673e50d3c
Arinze Ogbuniba <niklaus.oa@gmail.com>
2024-07-06 00:35:16 +0100

    Increase font size of nav links

--- src/components/DesktopMenu.jsx
+++ src/components/DesktopMenu.jsx
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom";
 
 const DesktopMenu = () => {
   return (
-    <div className="hidden lg:flex lg:gap-x-12">
-      <Link to="/" className="text-sm font-semibold leading-6 text-gray-900">
+    <div className="hidden md:flex md:gap-x-12">
+      <Link to="/" className="text-lg font-semibold leading-6 text-gray-900 hover:text-red-800 transition-colors duration-300">
         Home
       </Link>
-      <Link to="/about" className="text-sm font-semibold leading-6 text-gray-900">
+      <Link to="/about" className="text-lg font-semibold leading-6 text-gray-900 hover:text-red-800 transition-colors duration-300">
         About Us
       </Link>
-      <Link to="/contact" className="text-sm font-semibold leading-6 text-gray-900">
+      <Link to="/contact" className="text-lg font-semibold leading-6 text-gray-900 hover:text-red-800 transition-colors duration-300">
         Contact
       </Link>
       <Link to="/cart">
-        <IoCartOutline aria-hidden="true" className="text-3xl text-gray-900" />
+        <IoCartOutline aria-hidden="true" className="text-3xl text-gray-900 hover:text-red-900 transition-all duration-200 hover:scale-105" />
         <span className="sr-only">Cart</span>
       </Link>
     </div>
