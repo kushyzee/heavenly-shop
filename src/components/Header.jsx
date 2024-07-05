8526ff515f396ce420639ef520e32afd4f52d613
Arinze Ogbuniba <niklaus.oa@gmail.com>
2024-07-05 18:23:07 +0100

    Split into smaller components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,124 +1,49 @@
-import logo from '../assets/images/logo.svg'
-import { useState } from "react";
-import {
-  Dialog,
-  DialogPanel,
-  Popover,
-  PopoverButton,
-  PopoverGroup,
-  PopoverPanel,
-} from "@headlessui/react";
-import { FaBars, FaX } from "react-icons/fa6";
+import logo from "../assets/images/logo.svg";
+import { FaBars } from "react-icons/fa6";
 import { IoCartOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import DesktopMenu from "./DesktopMenu";
+import MobileMenu from "./MobileMenu";
 
 const Header = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
     <header className="bg-white">
+      <MobileMenu mobileMenuOpen={mobileMenuOpen} setMobileMenuOpen={setMobileMenuOpen} />
+
+      {/* Desktop navbar */}
       <nav
         aria-label="Global"
-        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        className="mx-auto flex max-w-7xl items-center justify-between p-4 lg:p-8"
       >
-        {/* Logo */}
-        <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Heavenly Delights</span>
-            <img
-              alt=""
-              src={logo}
-              className="h-7 w-auto"
-            />
-          </a>
-        </div>
+        <Link to="/">
+          <img
+            alt="Heavenly Delights"
+            src={logo}
+            className="h-6 w-auto lg:h-7"
+          />
+        </Link>
 
-        {/* Mobile menu */}
-        <div className="flex gap-x-5 lg:hidden">
+        <div className="flex gap-x-8 lg:hidden">
+          <IoCartOutline
+            className="text-3xl text-gray-900 cursor-pointer hover:scale-110 transition-transform duration-200"
+          />
+          <span className="sr-only">Cart</span>
+          {/* Mobile menu button*/}
           <button
             type="button"
             onClick={() => setMobileMenuOpen(true)}
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            className="inline-flex items-center justify-center text-gray-700"
           >
             <span className="sr-only">Open main menu</span>
             <FaBars aria-hidden="true" className="h-6 w-6" />
           </button>
-          {/* Cart icon*/}
-          <a href="#">
-            <IoCartOutline aria-hidden="true" className="text-3xl text-gray-900" />
-            <span className="sr-only">Cart</span>
-          </a>
         </div>
 
-        <PopoverGroup className="hidden lg:flex lg:gap-x-12">
-          <Popover className="relative">
-            <PopoverButton className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
-              Home
-            </PopoverButton>
-          </Popover>
-
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            About Us
-          </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Contact
-          </a>
-          <a href="#">
-            <IoCartOutline aria-hidden="true" className="text-3xl text-gray-900" />
-            <span className="sr-only">Cart</span>
-          </a>
-        </PopoverGroup>
+        <DesktopMenu />
       </nav>
-      <Dialog
-        open={mobileMenuOpen}
-        onClose={setMobileMenuOpen}
-        className="lg:hidden"
-      >
-        <div className="fixed inset-0 z-10" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-6/12 border-l border-gray-300 overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Heavenly Delights</span>
-              <img
-                alt=""
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                className="h-8 w-auto"
-              />
-            </a>
-            <button
-              type="button"
-              onClick={() => setMobileMenuOpen(false)}
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-            >
-              <span className="sr-only">Close menu</span>
-              <FaX aria-hidden="true" className="h-5 w-5" />
-            </button>
-          </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Home
-                </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  About Us
-                </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Contact
-                </a>
-              </div>
-            </div>
-          </div>
-        </DialogPanel>
-      </Dialog>
     </header>
   );
 };
