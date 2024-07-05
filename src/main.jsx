3e49ad97a750a957cb5ed64168e9e5c9c91d224a
Arinze Ogbuniba <niklaus.oa@gmail.com>
2024-07-05 18:22:42 +0100

    Add routes to other pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Layout from './components/layouts/Layout.jsx'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route path='/' element={<Layout />} >
-          {/* <Route index element={<App />} /> */}
+          <Route index element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='contact' element={<Contact />} />
         </Route>
       </Routes>
     </Router>
