0fdaec1408f745944d5bacf5291565d99c6ff2ff
Arinze Ogbuniba <niklaus.oa@gmail.com>
2024-07-06 00:32:56 +0100

    initial commit

---
+++ src/assets/team.js
@@ -0,0 +1,34 @@
+import emily from "./images/emily.png"
+import floyd from "./images/floyd.png"
+import jane from "./images/jane.png"
+import leslie from "./images/leslie.png"
+
+
+const Team = [
+  {
+    id: 1,
+    name: "Emily Brown",
+    position: 'Founder & Head Baker',
+    image: emily
+  },
+  {
+    id: 2,
+    name: "Floyd Jonathan",
+    position: 'Pastry Chef',
+    image: floyd
+  },
+  {
+    id: 3,
+    name: "Jane Dorsey",
+    position: 'Marketing Manager',
+    image: jane
+  },
+  {
+    id: 4,
+    name: "Leslie Granger",
+    position: 'Customer Service Manager',
+    image: leslie
+  },
+]
+
+export default Team;
