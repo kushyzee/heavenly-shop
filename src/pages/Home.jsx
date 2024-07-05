2919cf26dafdaaee04c6c3ba1e500dd750be1515
Arinze Ogbuniba <niklaus.oa@gmail.com>
2024-07-06 00:37:30 +0100

    Modify flexbox

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,27 +2,35 @@ import products from "../assets/products";
 
 const Home = () => {
   return (
-    <main className="px-5 mt-14 text-gray-700 text-base md:text-lg">
+    <main className="px-5 my-14 md:my-20 text-gray-700 text-base md:text-lg max-w-5xl mx-auto">
       <div>
-        <h2 className="text-3xl font-bold text-gray-900 ">Our Products</h2>
-        <p className="">
+        <h1 className="text-3xl lg:text-4xl font-bold text-gray-900 ">Our Products</h1>
+        <p className="text-lg mt-2">
           Explore our wide range of homemade cakes, cookies and pastries
         </p>
       </div>
 
       {/* Products */}
-      <section className="flex mt-9 max-w-4xl mx-auto gap-4 flex-wrap justify-start">
+      <section className="flex mt-9 max-w-4xl mx-auto flex-wrap justify-center items-center gap-y-2 md:gap-y-4">
         {products.map((product) => (
-          <div key={product.id} className="basis-40 max-w-60 grow border border-gray-300 rounded-lg">
+          <div
+            key={product.id}
+            className="w-1/2 p-2 md:p-4 grow bg-white md:w-1/3 shadow"
+          >
             <img
-              className="w-full h-36 object-cover object-center rounded-tl-lg rounded-tr-lg"
+              className="w-full rounded-md h-36 object-cover object-center"
               src={product.image}
               alt={product.name}
             />
-            <div className="py-3 px-3">
+            <div className="mt-3">
               <p>{product.name}</p>
               <p className="font-semibold text-2xl">{`$${product.price}.00`}</p>
-              <button className="mt-6 bg-red-600 text-white py-2 px-4 rounded-lg w-full hover:bg-red-700 transition-colors duration-200" type="button">Add to cart</button>
+              <button
+                className="mt-6 bg-red-600 text-white py-2 px-4 rounded-lg w-full hover:bg-red-700 transition-colors duration-200"
+                type="button"
+              >
+                Add to cart
+              </button>
             </div>
           </div>
         ))}
