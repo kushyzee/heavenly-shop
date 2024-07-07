efb47908b5d4c73e65b958079be215426e600e74
Arinze Ogbuniba <niklaus.oa@gmail.com>
2024-07-07 18:56:37 +0100

    Rework home.jsx to a landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,21 @@
-import products from "../assets/products";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <main className="px-5 my-14 md:my-20 text-gray-700 text-base md:text-lg max-w-5xl mx-auto">
-      <div>
-        <h1 className="text-3xl lg:text-4xl font-bold text-gray-900 ">Our Products</h1>
-        <p className="text-lg mt-2">
-          Explore our wide range of homemade cakes, cookies and pastries
-        </p>
+    <section className="">
+      <div className="bg-hero h-full bg-no-repeat bg-cover bg-center">
+        <div className=" text-neutral-50 py-44 px-5 bg-neutral-950/60 h-full">
+          <h1 className="text-4xl text-white font-bold z-20">
+            Indulge in Homemade Bliss at Heavenly Delights
+          </h1>
+          <p className="text-lg mt-2 mb-9">
+            Experience the heavenly taste of our homemade cakes, cookies, and
+            pastries. Made with love and the finest ingredients.
+          </p>
+          <Link to={"products"} className="bg-red-600 w-full inline-block text-center py-2.5 rounded-md"> Order Now</Link>
+        </div>
       </div>
-
-      {/* Products */}
-      <section className="flex mt-9 max-w-4xl mx-auto flex-wrap justify-center items-center gap-y-2 md:gap-y-4">
-        {products.map((product) => (
-          <div
-            key={product.id}
-            className="w-1/2 p-2 md:p-4 grow bg-white md:w-1/3 shadow"
-          >
-            <img
-              className="w-full rounded-md h-36 object-cover object-center"
-              src={product.image}
-              alt={product.name}
-            />
-            <div className="mt-3">
-              <p>{product.name}</p>
-              <p className="font-semibold text-2xl">{`$${product.price}.00`}</p>
-              <button
-                className="mt-6 bg-red-600 text-white py-2 px-4 rounded-lg w-full hover:bg-red-700 transition-colors duration-200"
-                type="button"
-              >
-                Add to cart
-              </button>
-            </div>
-          </div>
-        ))}
-      </section>
-    </main>
+    </section>
   );
 };
 
