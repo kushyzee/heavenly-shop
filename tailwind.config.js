1507886e034744de63ddef32068c0e5855c4140d
Arinze Ogbuniba <niklaus.oa@gmail.com>
2024-07-12 14:24:07 +0100

    Resolve path to background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        "hero": "url('./src/assets/images/hero-background.jpg')"
+        "hero": "url('/src/assets/images/hero-background.jpg')"
       }
     },
   },
